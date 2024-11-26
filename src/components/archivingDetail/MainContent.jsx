034f6e55252bc86b4f50d1e530e9d0b9a2fade8d
yundevingV <91381230+yundevingV@users.noreply.github.com>
2024-11-26 20:03:38 +0900

    feat : 11기 아카이빙 추가

--- src/components/archivingDetail/MainContent.jsx
+++ src/components/archivingDetail/MainContent.jsx
@@ -1,19 +1,22 @@
-import arch_test from "../../image/arch_test.avif";
+import aurum from "../../image/AURUM.jpg";
 import { FaReact, FaNodeJs, FaSwift, FaGithub, FaLink } from "react-icons/fa";
 import { FaSquareInstagram } from "react-icons/fa6";
 
 import { SiDjango, SiSpring } from "react-icons/si";
+import Icon from "./Icon.tsx";
 
 export default function MainContent() {
-  const skill = ["swift", "spring", "django"];
-  const people = ["나수아", "유지호", "박제영", "이윤성", "강지웅"];
+  const skill = ["react"];
+  const people = ["장광진", "서은", "박지원", "오혁준", "김채연"];
   return (
     <div className="w-full p-4 border-2 rounded-2xl  space-y-4 shadow-lg bg-white">
-      <img className="rounded-xl" src={arch_test} alt="x" />
-      <p className="font-bold text-2xl">도를 닦자</p>
+      <img className="rounded-xl mx-auto" src={aurum} alt="x" />
+      <p className="font-bold text-2xl">
+        아우름제 X 가톨릭대학교 멋쟁이사자처럼
+      </p>
       <p className="">
-        도를닦자프로젝트설명
-        도를닦자프로젝트설명도를닦자프로젝트설명도를닦자프로젝트설명도를닦자프로젝트설명도를닦자프로젝트설명도를닦자프로젝트설명도를닦자프로젝트설명{" "}
+        멋쟁이 사자처럼 11기 프론트엔드 개발팀에서 개발한 2023 아우름제 축제
+        페이지입니다.
       </p>
 
       <p className="font-bold text-2xl">⚒️ 기술스택</p>
@@ -46,9 +49,12 @@ export default function MainContent() {
       </div>
       <p className="font-bold text-2xl">🔗 링크</p>
       <div className="flex flex-wrap space-x-2">
-        <FaGithub size="26" />
-        <FaSquareInstagram size="26" />
-        <FaLink size="26" />
+        <Icon
+          href="https://github.com/CUK-LikeLion-Festival/festival_front"
+          icon={FaGithub}
+        />
+        <Icon icon={FaSquareInstagram} />
+        <Icon icon={FaLink} />
       </div>
     </div>
   );
